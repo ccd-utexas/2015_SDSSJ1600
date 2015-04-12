1ef8aa331ce3e277f1b64142b2ba229b40f1a9ae
stharrold <samuel.harrold@gmail.com>
2015-04-12 15:32:31 -0500

    utils: added phased light curve utilities

--- code/utils.py
+++ code/utils.py
@@ -413,3 +413,358 @@ def calc_best_period(times, fluxes, fluxes_err, candidate_periods,
         print("Relative Bayesian Information Criterion: {bic}".format(bic=best_bic))
     return best_period
 
+
+def calc_num_terms(times, fluxes, fluxes_err, best_period, max_n_terms=20, show_periodograms=False,
+                   show_summary_plots=True, period_unit='seconds', flux_unit='relative'):
+    """Calculate the number of Fourier terms that best represent the data's underlying
+    variability for representation by a multi-term generalized Lomb-Scargle periodogram.
+    Convenience function for methods from [1]_.
+       
+    Parameters
+    ----------
+    times : numpy.ndarray
+        1D array of time coordinates for data. Unit is time, e.g. seconds or days.
+    fluxes : numpy.ndarray
+        1D array of fluxes. Unit is integrated flux, e.g. relative flux or magnitudes.
+    fluxes_err : numpy.ndarray
+        1D array of errors for fluxes. Unit is same as `fluxes`.
+    best_period : float
+        Period that best represents the data. Unit is same as `times`.
+    max_n_terms : {20}, int, optional
+        Maximum number of terms to attempt fitting.
+        Example: From 10.3.3 of [1]_, many light curves of eclipses are well represented
+        with ~6 terms and are best fit with ~10 terms.
+    show_periodograms : {False, True}, bool, optional
+        If `False` (default), do not display periodograms (power vs period) for each candidate number of terms.
+    show_summary_plots : {True, False}, bool, optional
+        If `True` (default), display summary plots of delta BIC vs number of terms, periodogram and
+        phased light curve for best fit number of terms.
+    period_unit : {'seconds'}, string, optional
+    flux_unit : {'relative'}, string, optional
+        Strings describing period and flux units for labeling the plots.
+        Example: period_unit='seconds', flux_unit='relative' will label the x-axis with "Period (seconds)"
+        and label the y-axis with "Lomb-Scargle Power Spectral Density\n" +
+        "(from flux in relative, ang. freq. in 2*pi/seconds)".
+    
+    Returns
+    -------
+    best_n_terms : int
+        Number of Fourier terms that best fit the light curve. The number of terms
+        is determined by the maximum relative Bayesian Information Criterion, from
+        section 10.3.3 of [1]_.
+    phases : ndarray
+        The phase coordinates of the best-fit light curve. Unit is decimal orbital phase.
+    fits_phased : ndarray
+        The relative fluxes for the `phases` of the best-fit light curve. Unit is same as `fluxes`.
+    times_phased : ndarray
+        The phases of the corresponding input `times`. Unit is decimal orbital phase.
+
+    See Also
+    --------
+    calc_best_period, refine_best_period
+
+    Notes
+    -----
+    -  Range around the best period is based on the angular frequency resolution
+        of the original data. Adopted from [2]_.
+        acquisition_time = max(times) - min(times)
+        omega_resolution = 2.0 * np.pi / acquisition_time
+        num_omegas = 1000 # chosen to balance fast computation with medium range
+        anti_aliasing = 1.0 / 2.56 # remove digital aliasing
+        sampling_precision = 0.1 # ensure sampling precision is higher than data precision
+        range_omega_halfwidth = (num_omegas/2.0) * omega_resolution * anti_aliasing * sampling_precision
+    - Call after `calc_best_period`.
+    - Call before `refine_best_period`.
+
+    TODO
+    ----
+    TODO: Create test
+    
+    References
+    ----------
+    .. [1] Ivezic et al, 2014, Statistics, Data Mining, and Machine Learning in Astronomy
+    
+    """
+    # Calculate the multiterm periodograms and choose the best number of terms from the maximum relative BIC.
+    acquisition_time = max(times) - min(times)
+    omega_resolution = 2.0 * np.pi / acquisition_time
+    num_omegas = 1000 # chosen to balance fast computation with medium range
+    anti_aliasing = 1.0 / 2.56 # remove digital aliasing
+    sampling_precision = 0.1 # ensure sampling precision is higher than data precision
+    range_omega_halfwidth = (num_omegas/2.0) * omega_resolution * anti_aliasing * sampling_precision
+    max_period = 0.5 * acquisition_time
+    min_omega = 2.0 * np.pi / max_period
+    median_sampling_period = np.median(np.diff(times))
+    min_period = 2.0 * median_sampling_period
+    max_omega = 2.0 * np.pi / (min_period)
+    best_omega = 2.0 * np.pi / best_period
+    range_omegas = \
+        np.clip(
+            np.linspace(
+                start=best_omega - range_omega_halfwidth,
+                stop=best_omega + range_omega_halfwidth,
+                num=num_omegas, endpoint=True),
+            min_omega, max_omega)
+    range_periods = 2.0 * np.pi / range_omegas
+    nterms_bics = []
+    for n_terms in range(1, max_n_terms+1):
+        range_powers = astroML_ts.multiterm_periodogram(t=times, y=fluxes, dy=fluxes_err,
+                                                        omega=range_omegas, n_terms=n_terms)
+        range_bic_max = max(astroML_ts.lomb_scargle_BIC(P=range_powers, y=fluxes, dy=fluxes_err,
+                                                        n_harmonics=n_terms))
+        nterms_bics.append((n_terms, range_bic_max))
+        if show_periodograms:
+            print(80*'-')
+            plot_periodogram(periods=range_periods, powers=range_powers, xscale='linear', n_terms=n_terms,
+                             period_unit=period_unit, flux_unit=flux_unit, return_ax=False)
+            print("Number of Fourier terms: {num}".format(num=n_terms))
+            print("Relative Bayesian Information Criterion: {bic}".format(bic=range_bic_max))
+    # Choose the best number of Fourier terms from the maximum delta BIC.
+    best_idx = np.argmax(zip(*nterms_bics)[1])
+    (best_n_terms, best_bic) = nterms_bics[best_idx]
+    mtf = astroML_ts.MultiTermFit(omega=best_omega, n_terms=best_n_terms)
+    mtf.fit(t=times, y=fluxes, dy=fluxes_err)
+    (phases, fits_phased, times_phased) = mtf.predict(Nphase=1000, return_phased_times=True, adjust_offset=True)
+    if show_summary_plots:
+        # Plot delta BICs after all terms have been fit.
+        print(80*'-')
+        nterms_bics_t = zip(*nterms_bics)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(nterms_bics_t[0], nterms_bics_t[1], color='black', marker='o')
+        ax.set_xlim(min(nterms_bics_t[0]), max(nterms_bics_t[0]))
+        ax.set_title("Relative Bayesian Information Criterion vs\n" +
+                     "number of Fourier terms")
+        ax.set_xlabel("number of Fourier terms")
+        ax.set_ylabel("delta BIC")
+        plt.show()
+        range_powers = astroML_ts.multiterm_periodogram(t=times, y=fluxes, dy=fluxes_err,
+                                                        omega=range_omegas, n_terms=best_n_terms)
+        plot_periodogram(periods=range_periods, powers=range_powers, xscale='linear', n_terms=best_n_terms,
+                         period_unit=period_unit, flux_unit=flux_unit, return_ax=False)
+        print("Best number of Fourier terms: {num}".format(num=best_n_terms))
+        print("Relative Bayesian Information Criterion: {bic}".format(bic=best_bic))
+    return (best_n_terms, phases, fits_phased, times_phased)
+
+
+def plot_phased_light_curve(phases, fits_phased, times_phased, fluxes, fluxes_err, n_terms=1,
+                            flux_unit='relative', return_ax=False):
+    """Plot a phased light curve. Convenience function for
+    plot formats from [1]_.
+
+    Parameters
+    ----------
+    phases : ndarray
+        The phase coordinates of the best-fit light curve. Unit is decimal orbital phase.
+    fits_phased : ndarray
+        The relative fluxes for corresponding `phases` of the best-fit light curve.
+        Unit is integrated flux, e.g. relative flux or magnitudes.
+    times_phased : ndarray
+        The phases of the time coordinates for the observed data. Unit is decimal orbital phase.
+    fluxes : numpy.ndarray
+        1D array of fluxes corresponding to `times_phased`. Unit is integrated flux,
+        e.g. relative flux or magnitudes.
+    fluxes_err : numpy.ndarray
+        1D array of errors for fluxes. Unit is same as `fluxes`.
+    n_terms : {1}, int, optional
+        Number of Fourier terms used to fit the light curve.
+    flux_unit : {'relative'}, string, optional
+        String describing flux units for labeling the plot.
+        Example: flux_unit='relative' will label the y-axis with "Flux (relative)".
+    return_ax : {False, True}, bool
+        If `False` (default), show the periodogram plot. Return `None`.
+        If `True`, return a `matplotlib.axes` instance for additional modification.
+
+    Returns
+    -------
+    ax : matplotlib.axes
+        Returned only if `return_ax` is `True`. Otherwise returns `None`.
+    
+    See Also
+    --------
+    plot_periodogram
+    
+    Notes
+    -----
+    - The phased light curve is plotted through two complete cycles to illustrate the primary minimum.
+
+    TODO
+    ----
+    TODO: Create test from astroml book.
+
+    References
+    ----------
+    .. [1] Ivezic et al, 2014, Statistics, Data Mining, and Machine Learning in Astronomy
+    
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.errorbar(np.append(times_phased, np.add(times_phased, 1.0)), np.append(fluxes, fluxes),
+                np.append(fluxes_err, fluxes_err), fmt='.k', ecolor='gray', linewidth=1)
+    ax.plot(np.append(phases, np.add(phases, 1.0)), np.append(fits_phased, fits_phased), color='blue', linewidth=2)
+    ax.set_title(("Phased light curve\n" +
+                  "with {num} Fourier terms fit").format(num=n_terms))
+    ax.set_xlabel("Orbital phase (decimal)")
+    ax.set_ylabel("Flux ({unit})".format(unit=flux_unit))
+    if return_ax:
+        return_obj = ax
+    else:
+        plt.show()
+        return_obj = None
+    return return_obj
+
+
+def refine_best_period(times, fluxes, fluxes_err, best_period, n_terms=6,
+                       show_plots=True, period_unit='seconds', flux_unit='relative'):
+    """Refine the best period to a higher precision from a multi-term generalized Lomb-Scargle
+    periodogram. Convenience function for methods from [1]_.
+       
+    Parameters
+    ----------
+    times : numpy.ndarray
+        1D array of time coordinates for data. Unit is time, e.g. seconds or days.
+    fluxes : numpy.ndarray
+        1D array of fluxes. Unit is integrated flux, e.g. relative flux or magnitudes.
+    fluxes_err : numpy.ndarray
+        1D array of errors for fluxes. Unit is same as `fluxes`.
+    best_period : float
+        Period that best represents the data. Unit is same as `times`.
+    n_terms : {6}, int, optional
+        Number of Fourier terms to fit the light curve. To fit eclipses well often requires ~6 terms,
+        from section 10.3.3 of [1]_.
+    show_plots : {True, False}, bool, optional
+        If `True`, display plots of periodograms and phased light curves.
+    period_unit : {'seconds'}, string, optional
+    flux_unit : {'relative'}, string, optional
+        Strings describing period and flux units for labeling the plot.
+        Example: period_unit='seconds', flux_unit='relative' will label a periodogram
+        x-axis with "Period (seconds)" and label a periodogram y-axis with 
+        "Lomb-Scargle Power Spectral Density\n(from flux in relative, ang. freq. in 2*pi/seconds)".
+
+    
+    Returns
+    -------
+    refined_period : float
+        Refined period. Unit is same as `times`.
+    refined_period_resolution : float
+        Precision of refined period. Unit is same as `times`.
+    phases : ndarray
+        The phase coordinates of the best-fit light curve. Unit is decimal orbital phase.
+    fits_phased : ndarray
+        The relative fluxes for the `phases` of the best-fit light curve. Unit is same as `fluxes`.
+    times_phased : ndarray
+        The phases of the corresponding input `times`. Unit is decimal orbital phase.
+    mtf : astroML.time_series.MultiTermFit
+        Instance of the `astroML.time_series.MultiTermFit` class for the best fit model.
+
+    See Also
+    --------
+    calc_best_period, calc_num_terms
+
+    Notes
+    -----
+    -  Range around the best period is based on the angular frequency resolution
+        of the original data. Adopted from [2]_.
+        acquisition_time = max(times) - min(times)
+        omega_resolution = 2.0 * np.pi / acquisition_time
+        num_omegas = 2000 # chosen to balance fast computation with small range
+        anti_aliasing = 1.0 / 2.56 # remove digital aliasing
+        sampling_precision = 0.01 # ensure sampling precision very high relative to data precision
+        range_omega_halfwidth = (num_omegas/2.0) * omega_resolution * anti_aliasing * sampling_precision
+    - Resolution of refined period (period precision) is based on time resolution of original data.
+        max_period = 0.5 * acquisition_time
+        min_omega = 2.0 * np.pi / max_period
+        median_sampling_period = np.median(np.diff(times))
+        min_period = 2.0 * median_sampling_period
+        max_omega = 2.0 * np.pi / (min_period)
+        period_resolution = 2.0 * np.pi / (max_omega - min_omega)
+    - Call after `calc_num_terms`.
+
+    TODO
+    ----
+    TODO: Create test.
+    
+    References
+    ----------
+    .. [1] Ivezic et al, 2014, Statistics, Data Mining, and Machine Learning in Astronomy
+    .. [2] http://zone.ni.com/reference/en-XX/help/372416A-01/svtconcepts/fft_funda/
+    
+    """
+    # Calculate the multiterm periodogram and choose the best period from the maximal power.
+    acquisition_time = max(times) - min(times)
+    omega_resolution = 2.0 * np.pi / acquisition_time
+    num_omegas = 2000 # chosen to balance fast computation with medium range
+    anti_aliasing = 1.0 / 2.56 # remove digital aliasing
+    sampling_precision = 0.01 # ensure sampling precision very high relative to data precision
+    range_omega_halfwidth = (num_omegas/2.0) * omega_resolution * anti_aliasing * sampling_precision
+    max_period = 0.5 * acquisition_time
+    min_omega = 2.0 * np.pi / max_period
+    median_sampling_period = np.median(np.diff(times))
+    min_period = 2.0 * median_sampling_period
+    max_omega = 2.0 * np.pi / (min_period)
+    refined_period_resolution = 2.0 * np.pi / (max_omega - min_omega)
+    best_omega = 2.0 * np.pi / best_period
+    range_omegas = \
+        np.clip(
+            np.linspace(
+                start=best_omega - range_omega_halfwidth,
+                stop=best_omega + range_omega_halfwidth,
+                num=num_omegas, endpoint=True),
+            min_omega, max_omega)
+    range_periods = 2.0 * np.pi / range_omegas
+    range_powers = astroML_ts.multiterm_periodogram(t=times, y=fluxes, dy=fluxes_err,
+                                                    omega=range_omegas, n_terms=n_terms)
+    refined_omega = range_omegas[np.argmax(range_powers)]
+    refined_period = 2.0 * np.pi / refined_omega
+    mtf = astroML_ts.MultiTermFit(omega=refined_omega, n_terms=n_terms)
+    mtf.fit(t=times, y=fluxes, dy=fluxes_err)
+    (phases, fits_phased, times_phased) = mtf.predict(Nphase=1000, return_phased_times=True, adjust_offset=True)
+    if show_plots:
+        plot_periodogram(periods=range_periods, powers=range_powers, xscale='linear', n_terms=n_terms,
+                         period_unit=period_unit, flux_unit=flux_unit, return_ax=False)
+        plot_phased_light_curve(phases=phases, fits_phased=fits_phased, times_phased=times_phased,
+                                fluxes=fluxes, fluxes_err=fluxes_err, n_terms=n_terms,
+                                flux_unit=flux_unit, return_ax=False)
+        print("Refined period: {per} seconds".format(per=refined_period))
+        print("Refined period resolution: {per_res} seconds".format(per_res=refined_period_resolution))
+    return (refined_period, refined_period_resolution, phases, fits_phased, times_phased, mtf)
+
+
+def calc_flux_fits_residuals(phases, fits_phased, times_phased, fluxes):
+    """Calculate the fluxes and their residuals at phased times from a fit
+    to a light curve.
+    
+    Parameters
+    ----------
+    phases : numpy.ndarray
+        The phase coordinates of the best-fit light curve. Unit is decimal orbital phase.
+        Required: numpy.shape(phases) == numpy.shape(fits_phased)
+    fits_phased : numpy.ndarray
+        The relative fluxes for the `phases` of the best-fit light curve. Unit is relative flux.
+        Required: numpy.shape(phases) == numpy.shape(fits_phased)
+    times_phased : numpy.ndarray
+        The phases coordinates of the `fluxes`. Unit is decimal orbital phase.
+        Required: numpy.shape(times_phased) == numpy.shape(fluxes)
+    fluxes : numpy.ndarray
+        1D array of fluxes. Unit is integrated flux, e.g. relative flux or magnitudes.
+        Required: numpy.shape(times_phased) == numpy.shape(fluxes)
+
+    Returns
+    -------
+    fit_fluxes : numpy.ndarray
+        1D array of `fits_phased` resampled at `times_phased`.
+        numpy.shape(fluxes_fit) == numpy.shape(fluxes)
+    residuals : numpy.ndarray
+        1D array of the differences between `fluxes` and `fits_phased` resampled at `times_phased`:
+        residuals = fluxes - fits_phased_resampled
+        numpy.shape(residuals) == numpy.shape(fluxes)
+
+    TODO
+    ----
+    TODO: create test
+    
+    """
+    fit_fluxes = np.interp(x=times_phased, xp=phases, fp=fits_phased)
+    residuals = np.subtract(fluxes, fit_fluxes)
+    return (fit_fluxes, residuals)
+
